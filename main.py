2756a3988ed971c7d5493d36dd103f5e28da573c
Kyla Rieger <kyla.styles@gmail.com>
2017-12-22 11:03:06 -0500

    Modified add and remove functions to actually edit the watchlist, and added a 'movie already there' check.

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ terrible_movies = [
     "Starship Troopers"
 ]
 
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
+# ***The whole point of this app.***
+watchlist = []
+
+#def get_current_watchlist():
+    # returns user's current watchlist
+#    return watchlist
 
 @app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
     crossed_off_movie = request.form['crossed-off-movie']
 
-    if crossed_off_movie not in get_current_watchlist():
+    if crossed_off_movie not in watchlist:
         # the user tried to cross off a movie that isn't in their list,
         # so we redirect back to the front page and tell them what went wrong
         error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
@@ -31,7 +34,9 @@ def crossoff_movie():
         return redirect("/?error=" + error)
 
     # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
+    else:
+        watchlist.remove(crossed_off_movie)
+        return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
 
 @app.route("/add", methods=['POST'])
 def add_movie():
@@ -43,26 +48,26 @@ def add_movie():
         error = "Please specify the movie you want to add."
         return redirect("/?error=" + error)
 
+    # if the movie is already in the watchlist
+    elif new_movie in watchlist:
+        error = "{0} is already in your Watchlist. Pick another movie.".format(new_movie)
+        return redirect('/?error=' + error)
+
     # if the user wants to add a terrible movie, redirect and tell them the error
-    if new_movie in terrible_movies:
+    elif new_movie in terrible_movies:
         error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
         return redirect("/?error=" + error)
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
+    else:
+        new_movie_escaped = cgi.escape(new_movie, quote=True)
+        watchlist.append(new_movie_escaped)
+        return render_template('add-confirmation.html', movie=new_movie_escaped)
 
-    return render_template('add-confirmation.html', movie=new_movie)
-
-# TODO:
-# Change get_current_watchlist to return []. This simulates a user with an empty watchlist.
-# Modify edit.html to make sense in such a situation:
-#  First: Hide the <h2>My Watchlist</h2> and it's unordered list.
-#  Second: Hide the crossoff form, since there are no movies to cross off. 
-# Then you can change get_current_watchlist back to the list of hard-coded movies.
 
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('edit.html', watchlist=watchlist, error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
